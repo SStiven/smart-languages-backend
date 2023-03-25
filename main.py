1298f9b708dbb97dee19e6ad9197fe3fc016d50d
SStiven <svillegasc@protonmail.com>
2023-03-25 14:32:47 -0500

    Receive .wav from the swagger interface

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 from routers.chat import chat
 
 app = FastAPI()
 
+allowed_origins = [
+    "*",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=allowed_origins,
+    allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 app.include_router(chat.router, tags=["Chat"])
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- routers/chat/chat.py
+++ routers/chat/chat.py
@@ -1,9 +1,45 @@
-from fastapi import APIRouter, Depends
-from datetime import datetime
+from fastapi import APIRouter, UploadFile, File
+#from datetime import datetime
+from pydub import AudioSegment
+import soundfile as sf
+from io import BytesIO
+import openai
+import tempfile
+import os
+import pydub
+import wave
 
 router = APIRouter()
+openai.api_key = "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"
 
 @router.post("/chat/exchange")
-async def add_exchange(exchange: str):
-    request_time = datetime.now()
-    return {"time" : request_time}
+async def add_exchange(audio: UploadFile = File(...)):
+    audio_bytes = await audio.read()
+
+    # Determine the audio format from the file extension
+    file_ext = audio.filename.split(".")[-1].lower()
+    if file_ext == "mp3":
+        audio_segment = AudioSegment.from_file(BytesIO(audio_bytes), format="mp3")
+    elif file_ext == "wav":
+        audio_segment = AudioSegment.from_file(BytesIO(audio_bytes), format="wav")
+    elif file_ext == "ogg":
+        audio_segment = AudioSegment.from_file(BytesIO(audio_bytes), format="ogg")
+    else:
+        return {"error": "Unsupported audio format"}
+
+    # Export the audio to WAV format
+    wav_bytes = BytesIO()
+    output_format = "wav"
+    audio_segment.export(wav_bytes, format=output_format)
+
+    # Transcribe the audio
+    model = "whisper-1"
+    wav_file = BytesIO(wav_bytes.getvalue())
+    wav_file.name = audio.filename
+    transcript = openai.Audio.transcribe(
+        model=model,
+        file=wav_file,
+        content_type="audio/wav"
+    )
+
+    return {"response": transcript.text}
